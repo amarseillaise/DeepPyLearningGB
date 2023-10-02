31ee87d1397f45595a2fca0bcdbadae084d6e3c0
Marsel <tagrolka@yandex.ru>
2023-10-02 20:39:48 +0300

    Seminars 9 homework task 1 done

--- 9/Home/task1.py
+++ 9/Home/task1.py
@@ -8,45 +8,55 @@
 # функции в json файл.
 
 import csv
+import json
 from random import randint
 from typing import Callable
+from math import sqrt
+
+CSV_FILE = "random_file.csv"
+JSON_FILE = "random_file.json"
 
 
-def calculate_root_from_csv(func_csv: Callable):
-    def deco(func_root):
-        def wrapper():
-            with open(func_csv(), "r", encoding="UTF-8") as f:
+def calculate_root_from_csv(csv_generator: Callable):
+    def deco(func_root: Callable):
+        csv_generator()
+
+        def wrapper(*args, **kwargs):
+            result = {}
+            with open(CSV_FILE, "r", encoding="UTF-8") as f:
                 csv_reader = csv.reader(f, delimiter=";")
                 for row in csv_reader:
-                    root = func_root()!!!!!!!!!!!!!!!!!!!!!
-                    print(f"{row} -> {func_root}")
-
+                    a, b, c = row
+                    x1, x2 = func_root(int(a), int(b), int(c))
+                    result[f"{x1 = } ; {x2 = }"] = row
+            return result
         return wrapper
     return deco
 
 
+def roots_to_json(func: Callable):
+    def wrapper(*args, **kwargs):
+        with open(JSON_FILE, "w", encoding="UTF-8") as f:
+            json.dump(func(), f, indent=4)
+        return True
+    return wrapper
+
+
 def gen_csv():
-    file_name = "random_file.csv"
-    with open(file_name, "w", encoding="UTF-8") as f:
+    with open(CSV_FILE, "w", encoding="UTF-8") as f:
         csv_writer = csv.writer(f, delimiter=";", lineterminator="\n")
         csv_writer.writerows([[randint(-1000, 1000) for _ in range(3)] for _ in range(randint(100, 1000))])
-    return file_name
 
 
+@roots_to_json
 @calculate_root_from_csv(gen_csv)
-def finding_root(*, a: int | float, b: int | float, c: int | float) -> tuple[int | float | None, int | float | None]:
+def finding_root(a: int | float, b: int | float, c: int | float) -> tuple[int | float | None, int | float | None]:
     d = b ** 2 - 4 * a * c
     if d < 0:
         return None, None
-    x1 = (-b + d) / (2 * a)
-    x2 = (-b - d) / (2 * a)
+    x1 = (-b + sqrt(d)) / (2 * a)
+    x2 = (-b - sqrt(d)) / (2 * a)
     return x1, x2
 
 
 finding_root()
-
-
-
-
-# print(finding_root(a=4, b=-15, c=11))
-# gen_csv()
