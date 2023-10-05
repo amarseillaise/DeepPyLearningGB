07c90eec6fdf60116c9e4b2cfeaac5eb9714e24d
Marsel <tagrolka@yandex.ru>
2023-10-05 23:30:12 +0300

    Seminars 11 homework at work task 4

--- 11/Home/task4.py
+++ 11/Home/task4.py
@@ -16,3 +16,37 @@
 # Представление матрицы в виде строки для создания нового объекта. Реализуйте метод __repr__,
 # который возвращает строку, которую можно использовать для создания нового объекта класса Matrix.
 
+class Matrix:
+    def __init__(self, rows, cols):
+        self.rows = rows
+        self.cols = cols
+        self.__lst = [] if rows == 0 and cols == 0 else [[1 for _ in range(rows)] for _ in range(cols)]
+
+    def __add__(self, other):
+        lst = []
+        if isinstance(other, Matrix):
+            for i in range(self.rows):
+                temp_lst = []
+                for j in range(self.cols):
+                    temp = self.__lst[i][j] + other.__lst[i][j]
+                    temp_lst.append(temp)
+                lst.append(temp_lst)
+        new_instance = Matrix(0, 0)
+        new_instance.__lst = lst
+        new_instance.rows = self.rows
+        new_instance.cols = self.cols
+        return new_instance
+
+    def __str__(self):
+        lst = ""
+        for i in range(self.rows):
+            for j in range(self.cols):
+                lst += str(self.__lst[i][j]) + " "
+            lst += "\n"
+        return lst.replace(" \n", "\n")
+
+
+a = Matrix(3, 3)
+b = Matrix(3, 3)
+c = a + b
+print(c)
